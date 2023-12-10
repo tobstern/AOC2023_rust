ca55819cd9e40d1dcbb3de4df03bca38e9ae7da7
tobstern <tobiasphilipp11@gmail.com>
2023-12-10 02:48:50 +0100

    finished day04 part2 finally...different approach helps sometimes

--- day04/day04_p2.rs
+++ day04/day04_p2.rs
@@ -1,6 +1,5 @@
-use std::borrow::Borrow;
+use std::collections::HashMap;
 use std::fs;
-use std::{cell::RefCell, collections::HashMap};
 
 // #[derive(Debug)]
 pub fn read_txt(day: String) -> String {
@@ -12,6 +11,21 @@ pub fn read_txt(day: String) -> String {
     let text: String = fs::read_to_string(file_path).expect("Could not open the text-file");
 
     return text;
+}
+
+fn merge(mut owner: HashMap<i32, i32>, dummy: HashMap<i32, i32>) -> HashMap<i32, i32> {
+    // merges 2 HashMap together -> mutates the 1st:
+    for (key, val) in dummy {
+        // Option: is key already there? -> add to its value:
+        let owner_val: Option<_> = owner.get(&key);
+        let ret_val = match owner_val {
+            Some(x) => *x,
+            None => 0,
+        };
+
+        owner.insert(key, ret_val + val);
+    }
+    owner
 }
 
 fn has_won(curr_wins: Vec<&str>, wins: Vec<Vec<&str>>, mine: Vec<Vec<&str>>, curr: i32) -> i32 {
@@ -36,61 +50,9 @@ fn has_won(curr_wins: Vec<&str>, wins: Vec<Vec<&str>>, mine: Vec<Vec<&str>>, cur
     count
 }
 
-fn check_winning_cards(
-    // won_num: i32,
-    curr_card: i32,
-    instances: HashMap<i32, i32>,
-    wins: Vec<Vec<&str>>,
-    mine: Vec<Vec<&str>>,
-) -> () {
-    //-> HashMap<i32, i32> {
-    // check current card for wins:
-
-    // open newly found instance card:
-    let curr_wins: &Vec<&str> = &wins.clone()[(curr_card - 1) as usize];
-    // let next_won_num: i32 = has_won(curr_wins.clone(), wins.clone(), mine.clone(), next_curr);
-
-    let won_num: i32 = has_won(curr_wins.clone(), wins.clone(), mine.clone(), curr_card);
-
-    // print debug:
-    println!();
-    println!("instances (in check_winning_cards()): {:?}", &instances);
-    println!("current_card: {} - count of wins: {}", &curr_card, &won_num);
-
-    // last won number
-    if won_num < 1 {
-        // break off recursion
-        return; //&instances;
-    }
-
-    // let card_ids: Vec<i32> = Vec::new();
-    // create new instances:
-    for offset in 1..=won_num {
-        // save card id
-        let next_curr: i32 = curr_card + offset;
-
-        let ret_val: i32 = match instances.get(&next_curr) {
-            Some(x) => *x,
-            None => 0,
-        };
-
-        instances.insert(next_curr, ret_val + 1); // unwrap if not none else give 0!
-
-        check_winning_cards(
-            //next_won_num,
-            next_curr,
-            instances,
-            wins.clone(),
-            mine.clone(),
-        );
-    }
-    // consider maybe to return instances?
-    // &instances
-}
-
 fn main() {
-    // let day = String::from("04");
-    let day = String::from("04_test");
+    let day = String::from("04");
+    // let day = String::from("04_test");
 
     // read in the text-file
     let txt: String = read_txt(day);
@@ -99,12 +61,8 @@ fn main() {
 
     let line_vec: Vec<&str> = lines.collect();
 
-    let mut cards: Vec<Vec<String>> = vec![];
-    let mut wins_str: Vec<Vec<&str>> = vec![];
-    let mut whatihave_str: Vec<Vec<&str>> = vec![];
-
-    // the result sum
-    let mut sum: i32 = 0;
+    let mut wins_str: Vec<Vec<&str>> = Vec::from([]);
+    let mut whatihave_str: Vec<Vec<&str>> = Vec::from([]);
 
     // parse the input:
     for l in &line_vec {
@@ -135,45 +93,95 @@ fn main() {
         }
     }
     //
-    println!("wins_str: {:?}", &wins_str);
-    println!("whatihave_str: {:?}", &wins_str);
+    // println!("wins_str: {:?}", &wins_str);
+    // println!("whatihave_str: {:?}", &wins_str);
 
-    // recursive scratch cards:
+    // create HashMap with every win count per card:
+    let mut wins_per_card: HashMap<i32, i32> = HashMap::new();
+    let mut ind: usize = 0;
+    for (i, win_nums) in wins_str.iter().enumerate() {
+        // calc wins and add to HashMap
+
+        // start counting from 1:
+        ind = i + 1_usize;
+
+        let win_cnt: i32 = has_won(
+            win_nums.clone(),
+            wins_str.clone(),
+            whatihave_str.clone(),
+            ind as i32,
+        );
+
+        // save it
+        wins_per_card.insert(ind as i32, win_cnt);
+    }
+
+    // println!(
+    //     "wins per card {:?}, and the sum {}",
+    //     &wins_per_card,
+    //     &wins_per_card.values().sum::<i32>()
+    // );
+
+    // scratch cards:
     // number of winning nums -> "copies" next num cards -> go in each card again -> check winning cards and "copy"
     // -> until no cards are won!
     let mut instances: HashMap<i32, i32> = HashMap::new();
     let mut ind: usize = 0;
 
-    let temp_inst: HashMap<i32, i32> = instances.clone();
+    let mut current_wins: i32 = 0;
+    let mut queue: Vec<i32> = Vec::from([]);
 
     for (i, curr_wins) in wins_str.iter().enumerate() {
         // start counting from 1:
-        ind = i + 1_usize;
+        ind = i + 1_usize; // current card
+        let orig_card: i32 = ind as i32;
 
-        // loop through upper set:
-        let win_cnt: i32 = has_won(
-            curr_wins.clone(),
-            wins_str.clone(),
-            whatihave_str.clone(),
-            ind as i32,
-        );
+        let mut curr_card: i32 = 0;
 
-        temp_inst = instances.clone();
-        // check if returned value is None -> not yet set!
-        let ret_val: i32 = match temp_inst.get(&(ind as i32)) {
-            Some(x) => *x,
-            None => 0,
-        };
+        current_wins = *wins_per_card.get(&(ind as i32)).unwrap();
 
-        instances.insert(ind as i32, ret_val + win_cnt);
+        instances = merge(instances, HashMap::from([(orig_card, 1)]));
+        // println!(
+        //     "run {} instances {:?} sum {}",
+        //     &i,
+        //     &instances,
+        //     &instances.values().sum::<i32>()
+        // );
 
-        check_winning_cards(
-            // win_cnt,
-            ind as i32,
-            instances,
-            wins_str.clone(),
-            whatihave_str.clone(),
-        );
+        // loop while wins are found: - while queue is not empty
+        queue.push(orig_card); // current card
+        while queue.len() > 0 {
+            // loop each card in queue
+            // -> to find its wins and put the new/copied cards into queue,
+            // then go again through queue
+            curr_card = queue.pop().unwrap();
+            current_wins = *wins_per_card.get(&curr_card).unwrap();
+
+            // if current_wins < 1 {
+            //     // break while loop due to no wins:
+            //     break;
+            // }
+
+            // instances = merge(instances, HashMap::from([(curr_card), (current_wins)]));
+
+            // loop through every new instances (defined by current_wins)
+            for offset in 1..=current_wins {
+                // save card id
+                let next_curr: i32 = curr_card + offset;
+
+                // add next card to queue:
+                queue.push(next_curr);
+
+                instances = merge(instances, HashMap::from([(next_curr, 1)]));
+                // let temp = instances.clone();
+                // let ret_val: i32 = match temp.get(&next_curr) {
+                //     Some(x) => *x,
+                //     None => 0,
+                // };
+
+                // instances.insert(next_curr, ret_val + 1); // unwrap if not none else give 0!
+            }
+        }
     }
 
     // result:
